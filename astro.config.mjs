b6ede72f80e0235b2a84a3444c7c0331cded002c
tonyx94 <42481774+tonyx94@users.noreply.github.com>
2024-04-02 13:29:52 -0600

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
-    site: "https://tonyx94.github.io/"
+    site: "https://tonyx94.github.io/",
+    adapter: node({
+        mode: 'standalone'
+    })
 });
