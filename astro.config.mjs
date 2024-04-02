ccd7280ead17199faefcd92480cb7dd3586854a8
tonyx94 <42481774+tonyx94@users.noreply.github.com>
2024-04-02 13:33:26 -0600

    base

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
     site: "https://tonyx94.github.io/",
+    base: "inksublimex",
     adapter: node({
         mode: 'standalone'
     })
